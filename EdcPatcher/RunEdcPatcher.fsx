c58f1a105b8f2d853d39bd767a0d521c96e2c463
stephentetley <stephen.tetley@gmail.com>
2020-01-15 10:35:39 +0000

    Initial files added. All code previously developed in the  repository. Asset Patch has become big enough to demand its own repository.

---
+++ EdcPatcher/RunEdcPatcher.fsx
@@ -0,0 +1,89 @@
+﻿#r "netstandard"
+#r "System.Text.Encoding.dll"
+#r "System.Xml.Linq"
+#r "System.Xml.ReaderWriter"
+#r "System.Xml.XDocument"
+#r "System.IO.FileSystem.Primitives"
+open System.IO
+
+#I @"C:\Users\stephen\.nuget\packages\ExcelProvider\1.0.1\lib\netstandard2.0"
+#r "ExcelProvider.Runtime.dll"
+
+#I @"C:\Users\stephen\.nuget\packages\ExcelProvider\1.0.1\typeproviders\fsharp41\netstandard2.0"
+#r "ExcelDataReader.DataSet.dll"
+#r "ExcelDataReader.dll"
+#r "ExcelProvider.DesignTime.dll"
+
+#I @"C:\Users\stephen\.nuget\packages\system.io.packaging\4.5.0\lib\netstandard1.3"
+#r "System.IO.Packaging"
+#I @"C:\Users\stephen\.nuget\packages\DocumentFormat.OpenXml\2.9.1\lib\netstandard1.3"
+#r "DocumentFormat.OpenXml"
+
+
+#I @"C:\Users\stephen\.nuget\packages\FParsec\1.0.4-rc3\lib\netstandard1.6"
+#r "FParsec"
+#r "FParsecCS"
+
+
+open FSharp.Core
+
+#I @"C:\Users\stephen\.nuget\packages\slformat\1.0.2-alpha-20190721\lib\netstandard2.0"
+#r "SLFormat.dll"
+
+#I @"C:\Users\stephen\.nuget\packages\markdowndoc\1.0.1-alpha-20191014\lib\netstandard2.0"
+#r "MarkdownDoc.dll"
+
+#I @"C:\Users\stephen\.nuget\packages\sheetdoc\1.0.0-alpha-20191121a\lib\netstandard2.0"
+#r "SheetDoc.dll"
+
+
+#load "..\AssetPatch\src\AssetPatch\Base\Addendum.fs"
+#load "..\AssetPatch\src\AssetPatch\Base\Common.fs"
+#load "..\AssetPatch\src\AssetPatch\Base\AssocList.fs"
+#load "..\AssetPatch\src\AssetPatch\Base\ChangeFile.fs"
+#load "..\AssetPatch\src\AssetPatch\Base\Acronyms.fs"
+#load "..\AssetPatch\src\AssetPatch\Base\AbsChangeFile.fs"
+#load "..\AssetPatch\src\AssetPatch\Base\FuncLocPath.fs"
+#load "..\AssetPatch\src\AssetPatch\Base\Parser.fs"
+#load "..\AssetPatch\src\AssetPatch\Base\Printer.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\CommonTypes.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\PatchTypes.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\EquiIndexing.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\TemplateHierarchy.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\Template.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\CompilerMonad.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\PatchWriter.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\EmitCommon.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\EmitEquipment.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\EmitFuncLoc.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\Emitter.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplatePatcher\PatchCompiler.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplateCatalogue\Base.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplateCatalogue\AssetCondition.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplateCatalogue\Lstnut.fs"
+#load "..\AssetPatch\src\AssetPatch\TemplateCatalogue\Smonsy.fs"
+#load "..\AssetPatch\src\AssetPatch\Lib\Common.fs"
+#load "..\AssetPatch\src\AssetPatch\Lib\OSGB36.fs"
+#load "EdcPatcher\InputData.fs"
+#load "EdcPatcher\EdcTemplate.fs"
+#load "EdcPatcher\EdcPatcher.fs"
+open EdcPatcher.EdcPatcher
+
+
+
+let options : EdcPatcherOptions = 
+    {   UserName = "TETLEYS"
+        OutputDirectory = @"G:\work\Projects\assets\asset_patch\env_discharge_2019\patch_output"
+        WorkListPath = @"G:\work\Projects\assets\asset_patch\env_discharge_2019\EA_discharge_2019_worklist1.xlsx" 
+    }
+
+let main01 () = 
+    runEdcPatcherPhase1 options 
+
+
+// Generate ClassEqui and ValuaEqui files for Equipment 
+// once it has been activated and downloaded...
+let main02 () = 
+    let equiFile = @"G:\work\Projects\assets\asset_patch\env_discharge_worklist1_mocked_download.txt"
+    runEdcPatcherPhase2 options equiFile  
+
