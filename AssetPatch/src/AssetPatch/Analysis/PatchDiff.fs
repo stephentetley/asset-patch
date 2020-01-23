1143e4e739cdc446d8b98fa57e0ac065aae0392b
stephentetley <stephen.tetley@gmail.com>
2020-01-23 16:01:32 +0000

    Initial work on PatchDiff. Diffs are collected need to be reported in a readable form.

--- AssetPatch/src/AssetPatch/Analysis/PatchDiff.fs
+++ AssetPatch/src/AssetPatch/Analysis/PatchDiff.fs
@@ -1,9 +1,86 @@
-﻿// Copyright (c) Stephen Tetley 2019
+﻿// Copyright (c) Stephen Tetley 2020
 // License: BSD 3 Clause
 
 
-namespace AssetPatch.PatchAnalysis
+namespace AssetPatch.Analysis
 
 module PatchDiff = 
 
-    let hello() = "HELLO"
+    open AssetPatch.Base
+    open AssetPatch.Base.ChangeFile
+
+    type AssocDiff<'Key, 'T> = 
+        | AssocInLeft of 'Key * 'T
+        | AssocInRight of 'Key * 'T
+        | AssocSame of 'Key * 'T
+        | AssocDiff of 'Key * 'T *'T
+
+
+    let diffAssocLists (assocLeft: AssocList<string, string>)
+                       (assocRight: AssocList<string, string>) : AssocDiff<string, string> list = 
+        let keys : string list = 
+            Array.append (AssocList.keys assocLeft) (AssocList.keys assocRight) 
+                |> Array.toList
+                |> List.sort 
+                |> List.distinct
+        let rec work ks cont = 
+            match ks with
+            | [] -> cont []
+            | k1 :: rs -> 
+                match (AssocList.tryFind k1 assocLeft, AssocList.tryFind k1 assocRight) with
+                | None, None -> failwith "impossible"
+                | Some(a), Some(b) -> 
+                    if a = b then 
+                        work rs (fun xs -> cont (AssocSame(k1, a) :: xs))
+                    else work rs (fun xs -> cont (AssocDiff(k1, a, b) :: xs))
+                | None, Some(b) -> 
+                    work rs (fun xs -> cont (AssocInRight(k1, b) :: xs))
+                | Some(a), None -> 
+                    work rs (fun xs -> cont (AssocInLeft(k1, a) :: xs))
+        work keys (fun xs -> xs)
+
+    /// 'Key for Equi is EQUI field
+    type RowDiff<'Key> = 
+        | RowInLeft of 'Key
+        | RowInRight of 'Key 
+        | RowInBoth of 'Key * AssocDiff<string, string> list
+
+
+    /// This will degenerate if the key field is not in the rows of the ChangeFile!
+    let diffRows (sortKeyField: string) 
+                    (rowsLeft: AssocList<string, string> list)
+                    (rowsRight: AssocList<string, string> list) : RowDiff<string> list = 
+        let detGetKey = Option.defaultValue "_ERROR_" << AssocList.tryFind sortKeyField
+        let rows1 = List.sortBy detGetKey rowsLeft
+        let rows2 = List.sortBy detGetKey rowsRight
+        let rec work xs ys cont = 
+            match xs, ys with
+            | xs1, [] -> cont (List.map (RowInLeft << detGetKey) xs1)
+            | [], ys1 -> cont (List.map (RowInRight << detGetKey) ys1)
+            | x :: xs1, y :: ys1 -> 
+                let keyLeft = detGetKey x
+                match compare keyLeft (detGetKey y)  with
+                | i when i = 0 -> 
+                    // item is same row - check for changes 
+                    let innerDiff = diffAssocLists x y 
+                    work xs1 ys1 (fun vs -> RowInBoth(keyLeft, innerDiff) :: vs)
+                | i when i < 0 -> 
+                    // ys is in front of xs, so accumulate head of xs, still consider all of ys
+                    work xs1 ys (fun vs -> RowInLeft(keyLeft) :: vs)
+                | i when i > 0 -> 
+                    // xs is in front of ys, so accumulate head of ys, still consider all of xs
+                    work xs ys1 (fun vs -> RowInRight(keyLeft) :: vs)
+                | i -> failwithf "differenceL - Weird (impossible) pattern failure: %i" i
+        work rows1 rows2 (fun xs -> xs)
+
+
+    let diffChangeFiles (sortKeyField: string) 
+                        (fileLeft: ChangeFile)
+                        (fileRight: ChangeFile) : Result<RowDiff<string> list, string>  = 
+        if fileLeft.Header.EntityType <> fileRight.Header.EntityType then
+            Error "Mismatched file types"
+        else if fileLeft.ColumnHeaders <> fileRight.ColumnHeaders then
+            Error "Mismatched Attribute lists"
+        else 
+            diffRows sortKeyField (fileLeft.RowAssocs ()) (fileRight.RowAssocs ()) |> Ok
+
---
+++ AssetPatch/test/TestPatchDiff01.fsx
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Stephen Tetley 2019
+
+#r "netstandard"
+#r "System.Text.Encoding.dll"
+open System.IO
+
+#I @"C:\Users\stephen\.nuget\packages\FParsec\1.0.4-rc3\lib\netstandard1.6"
+#r "FParsec"
+#r "FParsecCS"
+
+
+open FSharp.Core
+
+#I @"C:\Users\stephen\.nuget\packages\slformat\1.0.2-alpha-20190721\lib\netstandard2.0"
+#r "SLFormat.dll"
+
+#I @"C:\Users\stephen\.nuget\packages\markdowndoc\1.0.1-alpha-20191014\lib\netstandard2.0"
+#r "MarkdownDoc.dll"
+
+#load "..\..\AssetPatch\src\AssetPatch\Base\Addendum.fs"
+#load "..\..\AssetPatch\src\AssetPatch\Base\Common.fs"
+#load "..\..\AssetPatch\src\AssetPatch\Base\AssocList.fs"
+#load "..\..\AssetPatch\src\AssetPatch\Base\ChangeFile.fs"
+#load "..\..\AssetPatch\src\AssetPatch\Base\Acronyms.fs"
+#load "..\..\AssetPatch\src\AssetPatch\Base\AbsChangeFile.fs"
+#load "..\..\AssetPatch\src\AssetPatch\Base\Parser.fs"
+#load "..\..\AssetPatch\src\AssetPatch\Base\Printer.fs"
+#load "..\..\AssetPatch\src\AssetPatch\Analysis\PatchDiff.fs"
+open AssetPatch.Base.ChangeFile
+open AssetPatch.Base.Parser
+open AssetPatch.Analysis.PatchDiff
+
+
+let readFiles (sourcePath1: string) (sourcePath2: string) : Result<ChangeFile * ChangeFile, string> = 
+    match readChangeFile sourcePath1 with
+    | Error msg1 -> Error msg1
+    | Ok ans1 -> 
+        match readChangeFile sourcePath2 with
+        | Error msg2 -> Error msg2
+        | Ok ans2 -> Ok (ans1, ans2)
+
+
+let reportDiffs01 () = 
+    let src1=  @"G:\work\Projects\assets\asset_patch\mmim_upgrade_2019\workings\preprod+outstation_retire+before_download.txt"
+    let src2 = @"G:\work\Projects\assets\asset_patch\mmim_upgrade_2019\workings\preprod+outstation_retire+after_download.txt"
+    match readFiles src1 src2 with
+    | Error msg -> Error msg
+    | Ok (a,b) -> diffChangeFiles "EQUI" a b
+
+
