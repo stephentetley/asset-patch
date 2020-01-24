26293ad1a15ae38b9fbf172226132e066070c603
stephentetley <stephen.tetley@gmail.com>
2020-01-24 11:10:27 +0000

    Work on Html report for Change File diffs.

--- AssetPatch/src/AssetPatch/Analysis/PatchDiff.fs
+++ AssetPatch/src/AssetPatch/Analysis/PatchDiff.fs
@@ -5,9 +5,14 @@
 namespace AssetPatch.Analysis
 
 module PatchDiff = 
+    
+    open System.IO
+
+    open Giraffe.GiraffeViewEngine
 
     open AssetPatch.Base
     open AssetPatch.Base.ChangeFile
+    open AssetPatch.Base.Parser
 
     type AssocDiff<'Key, 'T> = 
         | AssocInLeft of 'Key * 'T
@@ -63,15 +68,16 @@ module PatchDiff =
                 | i when i = 0 -> 
                     // item is same row - check for changes 
                     let innerDiff = diffAssocLists x y 
-                    work xs1 ys1 (fun vs -> RowInBoth(keyLeft, innerDiff) :: vs)
+                    work xs1 ys1 (fun vs -> cont(RowInBoth(keyLeft, innerDiff) :: vs))
                 | i when i < 0 -> 
                     // ys is in front of xs, so accumulate head of xs, still consider all of ys
-                    work xs1 ys (fun vs -> RowInLeft(keyLeft) :: vs)
+                    work xs1 ys (fun vs -> cont (RowInLeft(keyLeft) :: vs))
                 | i when i > 0 -> 
                     // xs is in front of ys, so accumulate head of ys, still consider all of xs
-                    work xs ys1 (fun vs -> RowInRight(keyLeft) :: vs)
+                    work xs ys1 (fun vs -> cont (RowInRight(keyLeft) :: vs))
                 | i -> failwithf "differenceL - Weird (impossible) pattern failure: %i" i
         work rows1 rows2 (fun xs -> xs)
+
 
 
     let diffChangeFiles (sortKeyField: string) 
@@ -83,4 +89,127 @@ module PatchDiff =
             Error "Mismatched Attribute lists"
         else 
             diffRows sortKeyField (fileLeft.RowAssocs ()) (fileRight.RowAssocs ()) |> Ok
+
+    // ************************************************************************
+    // Report
+
+
+    let private headerTable (tableTitle: string) (changeFile: ChangeFile): XmlNode = 
+        let dateValue = changeFile.Header.DateTime.ToString(format="yyyyMMdd")
+        let timeValue = changeFile.Header.DateTime.ToString(format="HHmmss")
+        table [] [
+            tbody [] [
+                tr [] [
+                    td [] [str tableTitle]
+                ]
+                tr [] [
+                    td [] [str "Data Model:"]
+                    td [] [changeFile.Header.DataModel.ToString() |> str]
+                ]
+                tr [] [
+                    td [] [str "Entity Type:"]
+                    td [] [changeFile.Header.EntityType.ToString() |> str]
+                ]
+                tr [] [
+                    td [] [str "Variant:"]
+                    td [] [changeFile.Header.Variant |> str]
+                ]
+                tr [] [
+                    td [] [str "User:"]
+                    td [] [changeFile.Header.User |> str]
+                ]
+                tr [] [
+                    td [] [str "Date:"]
+                    td [] [dateValue |> str]
+                ]
+                tr [] [
+                    td [] [str "Time:"]
+                    td [] [timeValue |> str]
+                ]
+            ]
+        ]
+
+    let assocDiffStr (diff: AssocDiff<string, string>): XmlNode option = 
+        match diff with
+        | AssocInLeft(name, value) -> sprintf "%s: missing in after '%s'" name value |> str |> Some
+        | AssocInRight(name, value) -> sprintf "%s: missing in before '%s'" name value |> str |> Some
+        | AssocSame(_,_) -> None
+        | AssocDiff(name, v1, v2) -> sprintf "%s: '%s' => '%s'" name v1 v2 |> str |> Some
+
+    let rowChangesTd (diffs: AssocDiff<string, string> list) : XmlNode = 
+        let texts = 
+            diffs 
+                |> List.map assocDiffStr
+                |> List.choose id
+
+        td [] texts
+        
+
+    let rowDiffTr (diff1: RowDiff<string>) : XmlNode = 
+        match diff1 with
+        | RowInLeft key -> 
+            tr [] [
+                td [] [
+                    sprintf "Error: Row %s missing after changes" key |> str
+                ]
+            ]
+        | RowInRight key -> 
+            tr [] [
+                td [] [
+                    sprintf "Error: Row %s added after changes" key |> str
+                ]
+            ]
+        | RowInBoth(key, diffs) -> 
+            tr [] [
+                td [] [
+                    sprintf "%s" key |> str
+                ] 
+                rowChangesTd diffs
+            ]
+
+    
+    let private diffsTableStats (diffs: RowDiff<string> list): XmlNode = 
+        p [] [sprintf "Row Count: %i" (List.length diffs) |> str]
+
+
+    let private diffsTable (diffs: RowDiff<string> list): XmlNode = 
+        table [] [
+            tbody [] (List.map rowDiffTr diffs)
+        ]
+
+
+    let private diffReport (before: ChangeFile) (after: ChangeFile) (diffs: RowDiff<string> list): XmlNode = 
+        html [] [
+            head [] [
+                title [] [str "Changes (before and after)"]
+                link [ 
+                    _rel "stylesheet"
+                    _type "text/css"
+                    _href "github.css"
+                ]
+            ]
+            body [] [
+                h1 [_id "changes-report"] [ str "Changes Report"]
+                headerTable "Before" before
+                headerTable "After" after
+                diffsTableStats diffs
+                diffsTable diffs
+            ]        
+        ]
+
+    let reportRowDiffs (sortfield: string) (beforeFilePath: string) (afterFilePath: string) (outputPath: string) : Result<Unit, string> = 
+        match readChangeFile beforeFilePath with
+        | Error msg1 -> Error msg1
+        | Ok beforeChanges -> 
+            match readChangeFile afterFilePath with
+            | Error msg2 -> Error msg2
+            | Ok afterChanges ->  
+                match diffChangeFiles sortfield beforeChanges afterChanges with
+                | Error msg -> Error msg
+                | Ok diffs -> 
+                    diffReport beforeChanges afterChanges diffs
+                        |> renderHtmlDocument
+                        |> fun x -> File.WriteAllText(path = outputPath, contents = x)
+                    Ok ()
+
 
--- AssetPatch/test/TestPatchDiff01.fsx
+++ AssetPatch/test/TestPatchDiff01.fsx
@@ -4,6 +4,9 @@
 #r "System.Text.Encoding.dll"
 open System.IO
 
+#I @"C:\Users\stephen\.nuget\packages\giraffe\4.0.1\lib\netstandard2.0"
+#r "Giraffe"
+
 #I @"C:\Users\stephen\.nuget\packages\FParsec\1.0.4-rc3\lib\netstandard1.6"
 #r "FParsec"
 #r "FParsecCS"
@@ -26,25 +29,15 @@ open FSharp.Core
 #load "..\..\AssetPatch\src\AssetPatch\Base\Parser.fs"
 #load "..\..\AssetPatch\src\AssetPatch\Base\Printer.fs"
 #load "..\..\AssetPatch\src\AssetPatch\Analysis\PatchDiff.fs"
-open AssetPatch.Base.ChangeFile
-open AssetPatch.Base.Parser
 open AssetPatch.Analysis.PatchDiff
 
 
-let readFiles (sourcePath1: string) (sourcePath2: string) : Result<ChangeFile * ChangeFile, string> = 
-    match readChangeFile sourcePath1 with
-    | Error msg1 -> Error msg1
-    | Ok ans1 -> 
-        match readChangeFile sourcePath2 with
-        | Error msg2 -> Error msg2
-        | Ok ans2 -> Ok (ans1, ans2)
+
 
 
 let reportDiffs01 () = 
     let src1=  @"G:\work\Projects\assets\asset_patch\mmim_upgrade_2019\workings\preprod+outstation_retire+before_download.txt"
     let src2 = @"G:\work\Projects\assets\asset_patch\mmim_upgrade_2019\workings\preprod+outstation_retire+after_download.txt"
-    match readFiles src1 src2 with
-    | Error msg -> Error msg
-    | Ok (a,b) -> diffChangeFiles "EQUI" a b
-
-
+    let dest = @"G:\work\Projects\assets\asset_patch\mmim_upgrade_2019\workings\html\preprod+outstation-retire-changes.html"
+    reportRowDiffs "EQUI" src1 src2 dest
+         
